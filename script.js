0716da7579642e9b31bcdb250b88387298361d28
Lescano713 <131562199+Lescano713@users.noreply.github.com>
2024-08-28 23:29:20 -0500

    uploading faqs from json and creating elements

---
+++ script.js
@@ -0,0 +1,27 @@
+const container = document.querySelector('#faqs-container');
+
+fetch('data.json')
+.then(res => res.json())
+.then(data => uploadingFaqs(data));
+
+function uploadingFaqs(data){
+    data.forEach(element => { 
+        const div = document.createElement('div');
+        div.classList.add('faqs')
+        const divQuestion = document.createElement('div');
+        divQuestion.classList.add('div-question');
+        const divAnswer = document.createElement('div');
+        divAnswer.classList.add('div-answer');
+        divQuestion.innerHTML = `
+        <h2>${element.question}</h2>
+        <div class="icon-container"></div>
+        `;
+
+        divAnswer.innerHTML =`
+        <p>${element.answer}</p>
+        `;
+
+        div.append(divQuestion,divAnswer);
+        container.appendChild(div);
+    });
+}
