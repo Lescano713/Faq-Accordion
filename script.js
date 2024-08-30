3e9cca04031592067981e43fe30785cc706c6a3f
Lescano713 <131562199+Lescano713@users.noreply.github.com>
2024-08-29 23:35:44 -0500

    adding eventListener to icon and button

--- script.js
+++ script.js
@@ -5,17 +5,25 @@ fetch('data.json')
 .then(data => uploadingFaqs(data));
 
 function uploadingFaqs(data){
-    data.forEach(element => { 
+    data.forEach((element, index) => { 
         const div = document.createElement('div');
         div.classList.add('faqs')
         const buttonQuestion = document.createElement('button');
         buttonQuestion.classList.add('button-question');
-        const divAnswer = document.createElement('div');
-        divAnswer.classList.add('div-answer');
+        buttonQuestion.type = 'button';
+        buttonQuestion.setAttribute('aria-expanded', 'false');
+        buttonQuestion.setAttribute('aria-controls',`answer-id${index}`)
+
         buttonQuestion.innerHTML = `
         <h2>${element.question}</h2>
         <div class="icon-container"></div>
         `;
+
+        const divAnswer = document.createElement('div');
+        divAnswer.classList.add('div-answer');
+        divAnswer.setAttribute('id', `answer${index}`)
+        divAnswer.setAttribute('aria-hidden', 'true');
+
         divAnswer.innerHTML =`
         <p>${element.answer}</p>
         `;
@@ -30,6 +38,13 @@ function hideShow(){
     icon.forEach(element=> 
         element.addEventListener('click', e =>{
             const divFaqs = e.target.closest('.faqs');
+            const divAnswer =divFaqs.querySelector('.div-answer');
+            const button = divFaqs.querySelector('button');
+            const isExpanded = button.getAttribute('aria-expanded') === 'true'
+
+            button.setAttribute('aria-expanded', !isExpanded);
+            divAnswer.setAttribute('aria-hidden', isExpanded ? 'true' : 'false');
+            
             divFaqs.classList.toggle('toggle');
 
         })
